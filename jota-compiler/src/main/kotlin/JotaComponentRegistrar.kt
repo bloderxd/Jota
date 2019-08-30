b2c4fc96534c7c9776481d4cfe57a7caff430a2e
Daniel Martins Leme <aptgetinstalldanielleme@gmail.com>
2019-08-29 23:19:54 -0300

    Import interceptors as values

--- jota-compiler/src/main/kotlin/JotaComponentRegistrar.kt
+++ jota-compiler/src/main/kotlin/JotaComponentRegistrar.kt
@@ -1,13 +1,12 @@
 import arrow.meta.extensions.ExtensionPhase
 import arrow.meta.extensions.MetaComponentRegistrar
 import com.google.auto.service.AutoService
-import matcher.DeclarativePatternMatchingPlugin
+import matcher.adt.adt
+import matcher.patternmatching.patternMatching
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 
 @AutoService(ComponentRegistrar::class)
 class JotaComponentRegistrar : MetaComponentRegistrar {
 
-    override fun intercept(): List<ExtensionPhase> = with(DeclarativePatternMatchingPlugin) {
-        adt() + patternMatching()
-    }
+    override fun intercept(): List<ExtensionPhase> = adt + patternMatching
 }
